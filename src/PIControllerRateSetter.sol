2efb3169c189363a9ebc329d7308bf87a1387924
oddaf <oddaf@proton.me>
2023-09-26 22:55:27 -0300

    making noise barrier public on PIControllerRateSetter

--- src/PIControllerRateSetter.sol
+++ src/PIControllerRateSetter.sol
@@ -18,22 +18,35 @@ pragma solidity 0.6.7;
 import "geb-treasury-reimbursement/math/GebMath.sol";
 
 abstract contract OracleLike {
-    function getResultWithValidity() virtual external view returns (uint256, bool);
+    function getResultWithValidity()
+        external
+        view
+        virtual
+        returns (uint256, bool);
 }
+
 abstract contract OracleRelayerLike {
-    function redemptionPrice() virtual external returns (uint256);
-    function modifyParameters(bytes32,uint256) virtual external;
+    function redemptionPrice() external virtual returns (uint256);
+
+    function modifyParameters(bytes32, uint256) external virtual;
 }
+
 abstract contract PIController {
-    function update(int256) virtual external returns (int256, int256, int256);
-    function perSecondIntegralLeak() virtual external view returns (uint256);
-    function elapsed() virtual external view returns (uint256);
-    function getNextPiOutput(int error) virtual public view returns (int256, int256, int256);
+    function update(int256) external virtual returns (int256, int256, int256);
+
+    function perSecondIntegralLeak() external view virtual returns (uint256);
+
+    function elapsed() external view virtual returns (uint256);
+
+    function getNextPiOutput(
+        int error
+    ) public view virtual returns (int256, int256, int256);
 }
 
 contract PIControllerRateSetter is GebMath {
     // --- Auth ---
-    mapping (address => uint) public authorizedAccounts;
+    mapping(address => uint) public authorizedAccounts;
+
     /**
      * @notice Add auth to an account
      * @param account Account to add auth to
@@ -42,6 +55,7 @@ contract PIControllerRateSetter is GebMath {
         authorizedAccounts[account] = 1;
         emit AddAuthorization(account);
     }
+
     /**
      * @notice Remove auth from an account
      * @param account Account to remove auth from
@@ -50,47 +64,46 @@ contract PIControllerRateSetter is GebMath {
         authorizedAccounts[account] = 0;
         emit RemoveAuthorization(account);
     }
+
     /**
-    * @notice Checks whether msg.sender can call an authed function
-    **/
-    modifier isAuthorized {
-        require(authorizedAccounts[msg.sender] == 1, "PIRateSetter/account-not-authorized");
+     * @notice Checks whether msg.sender can call an authed function
+     **/
+    modifier isAuthorized() {
+        require(
+            authorizedAccounts[msg.sender] == 1,
+            "PIRateSetter/account-not-authorized"
+        );
         _;
     }
 
     // --- Variables ---
     // When the price feed was last updated
-    uint256 public lastUpdateTime;                  // [timestamp]
+    uint256 public lastUpdateTime; // [timestamp]
     // Enforced gap between calls
-    uint256 public updateRateDelay;                 // [seconds]
+    uint256 public updateRateDelay; // [seconds]
 
     // --- System Dependencies ---
     // OSM or medianizer for the system coin
-    OracleLike                public orcl;
+    OracleLike public orcl;
     // OracleRelayer where the redemption price is stored
-    OracleRelayerLike         public oracleRelayer;
+    OracleRelayerLike public oracleRelayer;
     // Controller for the redemption rate
-    PIController            public piController;
+    PIController public piController;
     // The minimum percentage deviation from the redemption price that allows the contract
     // to calculate a non null redemption rate
-    uint256 noiseBarrier;                   // [TWENTY_SEVEN_DECIMAL_NUMBER]
+    uint256 public noiseBarrier; // [TWENTY_SEVEN_DECIMAL_NUMBER]
 
     // Constants
-    uint256 internal constant NEGATIVE_RATE_LIMIT = TWENTY_SEVEN_DECIMAL_NUMBER - 1;
+    uint256 internal constant NEGATIVE_RATE_LIMIT =
+        TWENTY_SEVEN_DECIMAL_NUMBER - 1;
     uint256 internal constant EIGHTEEN_DECIMAL_NUMBER = 10 ** 18;
     uint256 internal constant TWENTY_SEVEN_DECIMAL_NUMBER = 10 ** 27;
 
     // --- Events ---
     event AddAuthorization(address account);
     event RemoveAuthorization(address account);
-    event ModifyParameters(
-      bytes32 parameter,
-      address addr
-    );
-    event ModifyParameters(
-      bytes32 parameter,
-      uint256 val
-    );
+    event ModifyParameters(bytes32 parameter, address addr);
+    event ModifyParameters(bytes32 parameter, uint256 val);
     event UpdateRedemptionRate(
         uint marketPrice,
         uint redemptionPrice,
@@ -108,28 +121,35 @@ contract PIControllerRateSetter is GebMath {
     );
 
     constructor(
-      address oracleRelayer_,
-      address orcl_,
-      address piController_,
-      uint256 noiseBarrier_,
-      uint256 updateRateDelay_
+        address oracleRelayer_,
+        address orcl_,
+        address piController_,
+        uint256 noiseBarrier_,
+        uint256 updateRateDelay_
     ) public {
-        require(oracleRelayer_ != address(0), "PIRateSetter/null-oracle-relayer");
+        require(
+            oracleRelayer_ != address(0),
+            "PIRateSetter/null-oracle-relayer"
+        );
         require(orcl_ != address(0), "PIRateSetter/null-orcl");
         require(piController_ != address(0), "PIRateSetter/null-controller");
-        require(both(noiseBarrier_ >= 0, noiseBarrier_ <= 0.2E27), "PIRateSetter/invalid-noise-barrier");
+        require(
+            both(noiseBarrier_ >= 0, noiseBarrier_ <= 0.2E27),
+            "PIRateSetter/invalid-noise-barrier"
+        );
 
         authorizedAccounts[msg.sender] = 1;
 
-        oracleRelayer    = OracleRelayerLike(oracleRelayer_);
-        orcl             = OracleLike(orcl_);
-        piController    = PIController(piController_);
-        noiseBarrier                    = noiseBarrier_;
+        oracleRelayer = OracleRelayerLike(oracleRelayer_);
+        orcl = OracleLike(orcl_);
+        piController = PIController(piController_);
+        noiseBarrier = noiseBarrier_;
 
-        updateRateDelay  = updateRateDelay_;
+        updateRateDelay = updateRateDelay_;
 
         emit AddAuthorization(msg.sender);
         emit ModifyParameters("orcl", orcl_);
+        emit ModifyParameters("noiseBarrier", noiseBarrier_);
         emit ModifyParameters("oracleRelayer", oracleRelayer_);
         emit ModifyParameters("piController", piController_);
         emit ModifyParameters("updateRateDelay", updateRateDelay_);
@@ -137,65 +157,84 @@ contract PIControllerRateSetter is GebMath {
 
     // --- Boolean Logic ---
     function either(bool x, bool y) internal pure returns (bool z) {
-        assembly{ z := or(x, y)}
+        assembly {
+            z := or(x, y)
+        }
     }
+
     function both(bool x, bool y) internal pure returns (bool z) {
-        assembly{ z := and(x, y)}
+        assembly {
+            z := and(x, y)
+        }
     }
+
     function absolute(int x) internal pure returns (uint z) {
         z = (x < 0) ? uint(-x) : uint(x);
     }
+
     function addition(int256 a, int256 b) internal pure returns (int256) {
         int256 c = a + b;
-        require((b >= 0 && c >= a) || (b < 0 && c < a), "SignedSafeMath: addition overflow");
+        require(
+            (b >= 0 && c >= a) || (b < 0 && c < a),
+            "SignedSafeMath: addition overflow"
+        );
 
         return c;
     }
+
     function subtract(int256 a, int256 b) internal pure returns (int256) {
         int256 c = a - b;
-        require((b >= 0 && c <= a) || (b < 0 && c > a), "SignedSafeMath: subtraction overflow");
+        require(
+            (b >= 0 && c <= a) || (b < 0 && c > a),
+            "SignedSafeMath: subtraction overflow"
+        );
         return c;
     }
+
     // --- Management ---
     /*
-    * @notify Modify the address of a contract that the setter is connected to
-    * @param parameter Contract name
-    * @param addr The new contract address
-    */
-    function modifyParameters(bytes32 parameter, address addr) external isAuthorized {
+     * @notify Modify the address of a contract that the setter is connected to
+     * @param parameter Contract name
+     * @param addr The new contract address
+     */
+    function modifyParameters(
+        bytes32 parameter,
+        address addr
+    ) external isAuthorized {
         require(addr != address(0), "PIRateSetter/null-addr");
         if (parameter == "orcl") orcl = OracleLike(addr);
-        else if (parameter == "oracleRelayer") oracleRelayer = OracleRelayerLike(addr);
+        else if (parameter == "oracleRelayer")
+            oracleRelayer = OracleRelayerLike(addr);
         else if (parameter == "piController") {
-          piController = PIController(addr);
-        }
-        else revert("PIRateSetter/modify-unrecognized-param");
-        emit ModifyParameters(
-          parameter,
-          addr
-        );
+            piController = PIController(addr);
+        } else revert("PIRateSetter/modify-unrecognized-param");
+        emit ModifyParameters(parameter, addr);
     }
+
     /*
-    * @notify Modify a uint256 parameter
-    * @param parameter The parameter name
-    * @param val The new parameter value
-    */
-    function modifyParameters(bytes32 parameter, uint256 val) external isAuthorized {
+     * @notify Modify a uint256 parameter
+     * @param parameter The parameter name
+     * @param val The new parameter value
+     */
+    function modifyParameters(
+        bytes32 parameter,
+        uint256 val
+    ) external isAuthorized {
         if (parameter == "updateRateDelay") {
-          require(val > 0, "PIRateSetter/null-update-delay");
-          updateRateDelay = val;
-        }
-        else if (parameter == "noiseBarrier") {
-          require(both(val >= 0, val <= 0.2E27), "PIRateSetter/invalid-noise-barrier");
-          noiseBarrier = val;
-        }
-        else revert("PIRateSetter/modify-unrecognized-param");
-        emit ModifyParameters(
-          parameter,
-          val
-        );
+            require(val > 0, "PIRateSetter/null-update-delay");
+            updateRateDelay = val;
+        } else if (parameter == "noiseBarrier") {
+            require(
+                both(val >= 0, val <= 0.2E27),
+                "PIRateSetter/invalid-noise-barrier"
+            );
+            noiseBarrier = val;
+        } else revert("PIRateSetter/modify-unrecognized-param");
+        emit ModifyParameters(parameter, val);
     }
-    int256 constant private _INT256_MIN = -2**255;
+
+    int256 private constant _INT256_MIN = -2 ** 255;
+
     function multiply(int256 a, int256 b) internal pure returns (int256) {
         // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
         // benefit is lost if 'b' is also tested.
@@ -204,51 +243,67 @@ contract PIControllerRateSetter is GebMath {
             return 0;
         }
 
-        require(!(a == -1 && b == _INT256_MIN), "SignedSafeMath: multiplication overflow");
+        require(
+            !(a == -1 && b == _INT256_MIN),
+            "SignedSafeMath: multiplication overflow"
+        );
 
         int256 c = a * b;
         require(c / a == b, "SignedSafeMath: multiplication overflow");
 
         return c;
     }
+
     /*
-    * @notice Calculates relativeError = (reference-measured)/reference 
-    * @param measuredValue EIGHTEEEN_DECIMAL_NUMBER
-    * @param referenceValue TWENTY_SEVEN_DECIMAL_NUMBER
-    * @return relativeError TWENTY_SEVEN_DECIMAL_NUMBER
-    */
-    function relativeError(uint256 measuredValue, uint256 referenceValue) internal pure returns (int256) {
-        uint256 scaledMeasuredValue = multiply(measuredValue, 10**9);
-        int256 error = multiply(subtract(int(referenceValue), int(scaledMeasuredValue)),
-                                        int(TWENTY_SEVEN_DECIMAL_NUMBER)) / int(referenceValue);
+     * @notice Calculates relativeError = (reference-measured)/reference
+     * @param measuredValue EIGHTEEEN_DECIMAL_NUMBER
+     * @param referenceValue TWENTY_SEVEN_DECIMAL_NUMBER
+     * @return relativeError TWENTY_SEVEN_DECIMAL_NUMBER
+     */
+    function relativeError(
+        uint256 measuredValue,
+        uint256 referenceValue
+    ) internal pure returns (int256) {
+        uint256 scaledMeasuredValue = multiply(measuredValue, 10 ** 9);
+        int256 error = multiply(
+            subtract(int(referenceValue), int(scaledMeasuredValue)),
+            int(TWENTY_SEVEN_DECIMAL_NUMBER)
+        ) / int(referenceValue);
         return error;
     }
 
     /*
-    * @notice Convert per-second delta rate to per-second redemption rate
-    * @param piOutput TWENTY_SEVEN_DECIMAL_NUMBER
-    * @return redemptionRate TWENTY_SEVEN_DECIMAL_NUMBER
-    */
+     * @notice Convert per-second delta rate to per-second redemption rate
+     * @param piOutput TWENTY_SEVEN_DECIMAL_NUMBER
+     * @return redemptionRate TWENTY_SEVEN_DECIMAL_NUMBER
+     */
     function getRedemptionRate(int256 piOutput) public pure returns (uint) {
         return uint(addition(int(TWENTY_SEVEN_DECIMAL_NUMBER), piOutput));
     }
 
     /*
-    * @notice Relay a new redemption rate to the OracleRelayer
-    * @param redemptionRate The new redemption rate to relay TWENTY_SEVEN_DECIMAL_NUMBER
-    */
+     * @notice Relay a new redemption rate to the OracleRelayer
+     * @param redemptionRate The new redemption rate to relay TWENTY_SEVEN_DECIMAL_NUMBER
+     */
     function relayRate(uint256 redemptionRate) internal {
         oracleRelayer.modifyParameters("redemptionRate", redemptionRate);
     }
 
     /**
-    * @notice Compute and set a new redemption rate
-    **/
+     * @notice Compute and set a new redemption rate
+     **/
     function updateRate() external {
         // Check delay between calls
-        require(either(subtract(now, lastUpdateTime) >= updateRateDelay, lastUpdateTime == 0), "PIControllerRateSetter/wait-more");
+        require(
+            either(
+                subtract(now, lastUpdateTime) >= updateRateDelay,
+                lastUpdateTime == 0
+            ),
+            "PIControllerRateSetter/wait-more"
+        );
         // Get price feed updates
-        (uint256 marketPrice, bool hasValidValue) = orcl.getResultWithValidity();
+        (uint256 marketPrice, bool hasValidValue) = orcl
+            .getResultWithValidity();
         // If the oracle has a value
         require(hasValidValue, "PIControllerRateSetter/invalid-oracle-value");
         // If the price is non-zero
@@ -259,12 +314,14 @@ contract PIControllerRateSetter is GebMath {
         int256 error = relativeError(marketPrice, redemptionPrice);
 
         if (absolute(error) <= noiseBarrier) {
-          error = 0;
-        } 
+            error = 0;
+        }
 
         // Controller output is per-second 'delta rate' st.
         // 1 + output = per-second redemption rate
-        (int256 output, int256 pOutput, int256 iOutput) = piController.update(error);
+        (int256 output, int256 pOutput, int256 iOutput) = piController.update(
+            error
+        );
 
         uint newRedemptionRate = getRedemptionRate(output);
 
@@ -281,42 +338,49 @@ contract PIControllerRateSetter is GebMath {
             newRedemptionRate,
             pOutput,
             iOutput
-          );
-
+        );
     }
 
     // --- Getters ---
     /**
-    * @notice Get the market price from the system coin oracle
-    **/
+     * @notice Get the market price from the system coin oracle
+     **/
     function getMarketPrice() external view returns (uint256) {
         (uint256 marketPrice, ) = orcl.getResultWithValidity();
         return marketPrice;
     }
+
     /**
-    * @notice Get the redemption and the market prices for the system coin
-    **/
-    function getRedemptionAndMarketPrices() external returns (uint256 marketPrice, uint256 redemptionPrice) {
+     * @notice Get the redemption and the market prices for the system coin
+     **/
+    function getRedemptionAndMarketPrices()
+        external
+        returns (uint256 marketPrice, uint256 redemptionPrice)
+    {
         (marketPrice, ) = orcl.getResultWithValidity();
         redemptionPrice = oracleRelayer.redemptionPrice();
     }
 
     /*
-    * @notice Get next redemption rate
-    * @param marketPrice EIGHTEEEN_DECIMAL_NUMBER
-    * @param redemptionPrice TWENTY_SEVEN_DECIMAL_NUMBER
-    * @return nextRedemptionRate TWENTY_SEVEN_DECIMAL_NUMBER
-    */
-    function getNextRedemptionRate(uint256 marketPrice, uint256 redemptionPrice) public view returns (uint) {
+     * @notice Get next redemption rate
+     * @param marketPrice EIGHTEEEN_DECIMAL_NUMBER
+     * @param redemptionPrice TWENTY_SEVEN_DECIMAL_NUMBER
+     * @return nextRedemptionRate TWENTY_SEVEN_DECIMAL_NUMBER
+     */
+    function getNextRedemptionRate(
+        uint256 marketPrice,
+        uint256 redemptionPrice
+    ) public view returns (uint) {
         int256 error = relativeError(marketPrice, redemptionPrice);
 
         if (absolute(error) <= noiseBarrier) {
-          error = 0;
-        } 
+            error = 0;
+        }
 
         // Controller output is per-second 'delta rate' st.
         // 1 + output = per-second redemption rate
-        (int256 output, int256 pOutput, int256 iOutput) = piController.getNextPiOutput(error);
+        (int256 output, int256 pOutput, int256 iOutput) = piController
+            .getNextPiOutput(error);
 
         uint nextRedemptionRate = getRedemptionRate(output);
 
